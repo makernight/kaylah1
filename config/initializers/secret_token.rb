786a83049a765324c4f48df4c4a3e4039835e5ce
Maker Night <team@mouse.org>
2016-01-28 17:22:58 -0500

    Set up website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaylah1::Application.config.secret_key_base = 'b52476432ea375b767d4eff4c6f4e22d786191d66f606e4c006af250cf35b15187a59ebbdf12516f88425df98aeb47a38fcfe87b48638b0b0c8da00a0efe3742'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kaylah1::Application.config.session_store :cookie_store, key: '_kaylah1_session'
